25984cd0401203ed64eca6e52653811d5fa1eb34
Ruben Maurer <rubenmaurer@live.de>
2022-03-11 11:52:14 +0100

    Adding mime_type validation.

--- onyx/backend/onyx/schema/request/PersistenceRequestSchema.py
+++ onyx/backend/onyx/schema/request/PersistenceRequestSchema.py
@@ -1,7 +1,7 @@
-from marshmallow import fields
+from marshmallow import fields, validate
 
 from onyx.schema.request.GeneratorRequestSchema import GeneratorRequestSchema
 
 
 class PersistenceRequestSchema(GeneratorRequestSchema):
-    mime_type = fields.String(default='plain/text', required=True, desciption='The mime-type of the submitted payload.')
+    mime_type = fields.String(default='plain/text', required=True, validate=validate.OneOf(['text/plain', 'image/png;base64', 'image/jpeg;base64']), desciption='The mime-type of the submitted payload.')
